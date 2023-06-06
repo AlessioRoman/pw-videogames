4ab0d740480be7cd01969a8b2aa8baf2aed62bf5
Damianopipitone <damianopipitone33@gmail.com>
2023-06-06 10:50:30 +0200

    Added new VideogameModel Class

---
+++ pw-videogames/Models/VideogameModel.cs
@@ -0,0 +1,26 @@
+﻿namespace pw_videogames.Models
+{
+    public class VideogameModel
+    {
+        public int Id {  get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImgUrl { get; set; }
+
+        public float Price { get; set; }
+
+        public VideogameModel() { }
+
+        public VideogameModel(int id, string name, string description, string imgUrl, float price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            ImgUrl = imgUrl;
+            Price = price;
+        }
+    }
+}
