d69771fc3db6c1f014179e67842559629b569997
Khodazone <khodazone@gmail.com>
2023-06-06 11:59:47 +0200

    fix3

--- pw-videogames/Controllers/VideogameController.cs
+++ pw-videogames/Controllers/VideogameController.cs
@@ -8,39 +8,36 @@ namespace pw_videogames.Controllers
         {
             using (VideogameContext db = new VideogameContext())
             {
-                List<Videogame> vgList = db.Videogame.ToList();
-                return View(vgList);
+                List<VideogameModel> videogame = db.Videogame.ToList();
+
+                return View(videogame);
+            }
+        }
+
+        // ACTION CREATE
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(VideogameModel newVideogame)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(newVideogame);
+            }
+            else
+            {
+                using (VideogameContext db = new VideogameContext())
+                {
+                    db.Books.Add(newVideogame);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
             }
         }
     }
-
-    // ACTION CREATE
-
-    [HttpGet]
-    public IActionResult Create()
-    {
-        return View();
-    }
-
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public IActionResult Create(Pizza newVideogame)
-    {
-        if (!ModelState.IsValid)
-        {
-            return View("Create", newVideogame);
-        }
-
-        using (VideogameContext db = new VideogameContext())
-        {
-            db.Pizza.Add(newVideogame);
-            db.SaveChanges();
-
-            return RedirectToAction("Index");
-        }
-    }
-
-    // ACTION UPDATE
 
 }
 
