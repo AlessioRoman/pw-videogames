68c965f5242bf9a80a6b6ee9ed3abf824dce1cad
Khodazone <khodazone@gmail.com>
2023-06-06 11:06:28 +0200

    controller index

---
+++ pw-videogames/Controllers/VideogameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pw_videogames.Controllers
+{
+    public class VideogameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
