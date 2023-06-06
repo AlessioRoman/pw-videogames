793f8cd7a5a3a1d7ab485f01cadc7fc1feb17c4d
Damianopipitone <damianopipitone33@gmail.com>
2023-06-06 11:55:48 +0200

    Added new migration and database

--- pw-videogames/Models/VideogameModel.cs
+++ pw-videogames/Models/VideogameModel.cs
@@ -1,13 +1,20 @@
-﻿namespace pw_videogames.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace pw_videogames.Models
 {
     public class VideogameModel
     {
+        [MaxLength(50)]
         public int Id {  get; set; }
-
+        
+        [MaxLength(400)]
         public string Name { get; set; }
-
+        
+        [MaxLength(500)]
         public string Description { get; set; }
 
+        [Url]
         public string ImgUrl { get; set; }
 
         public float Price { get; set; }
