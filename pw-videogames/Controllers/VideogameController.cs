9f4f5d98a972803f242a6b0a336fe1b0ab6efdbb
Khodazone <khodazone@gmail.com>
2023-06-06 11:50:11 +0200

    fix2

--- pw-videogames/Controllers/VideogameController.cs
+++ pw-videogames/Controllers/VideogameController.cs
@@ -13,4 +13,35 @@ namespace pw_videogames.Controllers
             }
         }
     }
+
+    // ACTION CREATE
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Pizza newVideogame)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View("Create", newVideogame);
+        }
+
+        using (VideogameContext db = new VideogameContext())
+        {
+            db.Pizza.Add(newVideogame);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+
+    // ACTION UPDATE
+
 }
+
+
