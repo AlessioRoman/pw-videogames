4e969f971080243fa5687a36aeeeb16bad0d7b62
AlessioRoman <alessio.romano02dev@gmail.com>
2023-06-07 11:28:33 +0200

    Added transaction logic

--- pw-videogames/Controllers/CustomerController.cs
+++ pw-videogames/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata;
 using pw_videogames.Database;
 using pw_videogames.Models;
 
@@ -27,6 +28,30 @@ namespace pw_videogames.Controllers
                 } else
                 {
                     return NotFound($"Il videogioco con id {id} non è stato trovato!");
+                }
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Transaction(int id, int quantity) 
+        {
+            using (VideogameContext db = new VideogameContext())
+            {
+                VideogameModel? videogameToBuy = db.Videogames.Where(videogame => videogame.Id == id).FirstOrDefault();
+                if (videogameToBuy != null)
+                {
+                    TransactionModel newTransaction = new TransactionModel();
+                    newTransaction.Videogame = videogameToBuy;
+                    newTransaction.Date = DateTime.Now;
+                    newTransaction.Quantity = quantity;
+                    db.Add(newTransaction);
+                    db.SaveChanges();
+
+                    return View("Index");
+                }
+                else
+                {
+                    return NotFound("transaction error");
                 }
             }
         }
